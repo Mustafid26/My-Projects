fb49d28f79a96a86bd3119db4e7644e30f2947e8
unknown <mustafid.mk@gmail.com>
2022-09-10 11:41:11 +0700

    first commit

---
+++ Latihan C/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Biodata Diri\n");
+    printf("=======================\n");
+    printf("Nama   : Mustafid Kaisalana\n");
+    printf("NIM    : A11.2022.14703\n");
+    printf("Alamat : Perum Manunggal 01 Kabupaten Semarang\n");
+    printf("Hobi   : Mendengarkan Musik\n");
+    printf("=======================\n");
+    return 0;
+}
